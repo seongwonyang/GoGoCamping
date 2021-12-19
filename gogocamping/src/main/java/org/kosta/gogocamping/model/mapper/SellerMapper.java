abb7aa7b08282d3cf1521221ec3f6b87ca671224
Liiims <sehee167@naver.com>
2021-12-19 18:54:39 +0900

    Update SellerMapper
    - 브랜드 리스트 조회

--- gogocamping/src/main/java/org/kosta/gogocamping/model/mapper/SellerMapper.java
+++ gogocamping/src/main/java/org/kosta/gogocamping/model/mapper/SellerMapper.java
@@ -1,8 +1,13 @@
 package org.kosta.gogocamping.model.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.kosta.gogocamping.model.domain.SellerVO;
 
 @Mapper
 public interface SellerMapper {
+
+	List<SellerVO> getAllBrandList();
 
 }
