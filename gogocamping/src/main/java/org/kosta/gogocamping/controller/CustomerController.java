54e2d0e7cffe7563c30d2ff6030e0b90daa70ccd
Liiims <sehee167@naver.com>
2021-12-24 12:08:44 +0900

    Update CustomerController
    - registerCustomer 메소드에 상품 정보 추가

--- gogocamping/src/main/java/org/kosta/gogocamping/controller/CustomerController.java
+++ gogocamping/src/main/java/org/kosta/gogocamping/controller/CustomerController.java
@@ -1,19 +1,22 @@
 package org.kosta.gogocamping.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
 import org.kosta.gogocamping.model.domain.CustomerVO;
+import org.kosta.gogocamping.model.domain.PagingBean;
 import org.kosta.gogocamping.model.mapper.CategoryMapper;
 import org.kosta.gogocamping.model.mapper.CustomerMapper;
+import org.kosta.gogocamping.model.mapper.ProductMapper;
 import org.kosta.gogocamping.model.mapper.SellerMapper;
 import org.kosta.gogocamping.model.service.MailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 @Controller
 public class CustomerController {
@@ -25,6 +28,8 @@ public class CustomerController {
 	private SellerMapper sellerMapper;
 	@Resource
 	private CategoryMapper categoryMapper;
+	@Resource
+	private ProductMapper productMapper;
 	
 	@RequestMapping("loginCustomerForm")
 	public String loginCustomerForm(Model model) {
@@ -72,8 +77,23 @@ public class CustomerController {
 		}
 	}
 	@RequestMapping("registerCustomer") 
-	public String registerCustomer(String customerId, String customerPassword, String customerName, String customerEmail, String customerTel, String customerBirth, String customerPostNumber, String customerAddress, String customerDetailedAddress) {
+	public String registerCustomer(String customerId, String customerPassword, String customerName, String customerEmail, String customerTel, String customerBirth, String customerPostNumber, String customerAddress, String customerDetailedAddress, Model model) {
 		customerMapper.registerCustomer(customerId, customerPassword, customerName, customerEmail, customerTel, customerBirth, customerPostNumber, customerAddress, customerDetailedAddress);
+		int totalCount = productMapper.getAllProductCount();
+		PagingBean pagingBean = new PagingBean(totalCount);
+
+		int startRowNumber = pagingBean.getStartRowNumber();
+		int endRowNumber = pagingBean.getEndRowNumber();
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("startRowNumber", startRowNumber);
+		map.put("endRowNumber", endRowNumber);
+		map.put("option", "");
+		
+		model.addAttribute("pagingBean", pagingBean);
+		model.addAttribute("allProductList", productMapper.getAllProductList(map)); // 전체 상품 리스트
+		model.addAttribute("allBrandList", sellerMapper.getAllBrandList()); // 전체 브랜드 리스트
+		model.addAttribute("categoryList", categoryMapper.getCategoryList()); // 전체 카테고리 리스트
 		return "home.tiles";
 	}
 
