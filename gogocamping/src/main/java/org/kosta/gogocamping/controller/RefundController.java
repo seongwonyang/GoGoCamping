a2b241e5c7e9fe2f9b20f9e1d5f5f645ab395834
Liiims <sehee167@naver.com>
2022-01-03 13:54:43 +0900

    Fix refund
    - 교환/환불 에러 해결

--- gogocamping/src/main/java/org/kosta/gogocamping/controller/RefundController.java
+++ gogocamping/src/main/java/org/kosta/gogocamping/controller/RefundController.java
@@ -6,16 +6,20 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.kosta.gogocamping.model.domain.CategoryVO;
 import org.kosta.gogocamping.model.domain.CustomerVO;
 import org.kosta.gogocamping.model.domain.OrderDetailVO;
 import org.kosta.gogocamping.model.domain.RefundVO;
+import org.kosta.gogocamping.model.domain.SellerVO;
 import org.kosta.gogocamping.model.mapper.CategoryMapper;
 import org.kosta.gogocamping.model.mapper.CustomerMapper;
 import org.kosta.gogocamping.model.mapper.OrderMapper;
 import org.kosta.gogocamping.model.mapper.RefundMapper;
 import org.kosta.gogocamping.model.mapper.SellerMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,37 +36,39 @@ public class RefundController{
 	@Resource
 	private CustomerMapper customerMapper;
 	
+	@ModelAttribute("allBrandList")
+	public List<SellerVO> allBrandList() {
+		return sellerMapper.getAllBrandList(); // 전체 브랜드 리스트
+	}
+	@ModelAttribute("categoryList")
+	public List<CategoryVO> categoryList() {
+		return categoryMapper.getCategoryList(); // 전체 카테고리 리스트
+	}
+	
 	@RequestMapping("/writePage")
-	public String refund(HttpServletRequest request, int orderDetailNo, Model model) {
-		//HttpSession session = request.getSession(false);
-		//CustomerVO customerVO = (CustomerVO) session.getAttribute("loginVO");
-		
+	public String refund(int orderDetailNo, Model model) {
 		model.addAttribute("refundProduct", orderMapper.getProductInfoByOrderDetailNo(orderDetailNo));
-		model.addAttribute("allBrandList", sellerMapper.getAllBrandList()); // 전체 브랜드 리스트
-		model.addAttribute("categoryList", categoryMapper.getCategoryList()); // 전체 카테고리 리스트
 		
 		return "customer/refund.tiles";
 	}
 	
+	@Transactional
 	@RequestMapping("/write")
 	public String insertRefund(HttpServletRequest request, RefundVO refundVO, int orderDetailNo, Model model) {
 		HttpSession session = request.getSession(false);
 		CustomerVO customerVO = (CustomerVO) session.getAttribute("loginVO");
-		
-		System.out.println(refundVO);
-		
+
 		OrderDetailVO orderDetailVO = new OrderDetailVO();
 		orderDetailVO.setOrderDetailNo(orderDetailNo);
 		
 		refundVO.setOrderDetailVO(orderDetailVO);
+		
+		System.out.println("refundVO: " + refundVO);
+		
 		refundMapper.insertRefund(refundVO); // 환불 테이블 삽입
 		orderMapper.updateStatusOrderDetail(orderDetailNo);
-		
-		List<OrderDetailVO> list = orderMapper.selectOrderList(customerVO.getCustomerId());
-		model.addAttribute("orderList", list);
-		model.addAttribute("allBrandList", sellerMapper.getAllBrandList()); // 전체 브랜드 리스트
-		model.addAttribute("categoryList", categoryMapper.getCategoryList()); // 전체 카테고리 리스트
-		return "redirect:/order/orderCheck";
+
+		return "redirect:/order/orderCheck?customerId="+customerVO.getCustomerId();
 	}
 	
 }
